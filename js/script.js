19f04c9f1d09732ce43a79341344d8468bbd0cc3
Jakob Hjalmarsson <jakob.hjalmarsson@utb.ecutbildning.se>
2024-09-27 18:51:37 +0200

    Refactored dark mode implementation

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ const phoneMiddle = document.querySelector('.phone-middle');
 const phoneScreensSide = document.querySelectorAll('.phone-side');
 const instructionsHeading = document.querySelector('#instructions-heading');
 const instructionsParagraph = document.querySelector('#instructions-paragraph');
+const activeQuestion = document.querySelector('#active-question');
 
 button.addEventListener('click', () => {
   if (document.body.classList.contains('dark-mode')) {
@@ -23,6 +24,7 @@ button.addEventListener('click', () => {
       }
     });
     mail.setAttribute('src', '/images/mail-light.svg');
+    activeQuestion.classList.remove('active-question');
   } else {
     hamburger.setAttribute('src', '/images/hamburger-menu-dark.svg');
     appStore.setAttribute('src', '/images/appstore-dark.svg');
@@ -34,19 +36,23 @@ button.addEventListener('click', () => {
       }
     });
     mail.setAttribute('src', '/images/mail-dark.svg');
+    if (mediaQueryTablet.matches) {
+      activeQuestion.classList.add('active-question');
+    }
   }
   
   document.body.classList.toggle('dark-mode');
 });
 
-function changeActiveFaqArrow(mediaQuery) {
+function changeActiveFaq(mediaQuery) {
   if (mediaQuery.matches) {
     if (document.body.classList.contains('dark-mode')) {
       faqArrow[0].setAttribute('src', '/images/faq-down-arrow-dark.svg');
+      activeQuestion.classList.add('active-question');
     } else {
       faqArrow[0].setAttribute('src', '/images/faq-down-arrow-light.svg');
     }
-
+    
     faqArrow[2].setAttribute('src', '/images/faq-up-arrow.svg');
     
     faqArrow[0].classList.remove('active-arrow');
@@ -56,6 +62,7 @@ function changeActiveFaqArrow(mediaQuery) {
   } else {
     if (document.body.classList.contains('dark-mode')) {
       faqArrow[2].setAttribute('src', '/images/faq-down-arrow-dark.svg');
+      activeQuestion.classList.remove('active-question');
     } else {
       faqArrow[2].setAttribute('src', '/images/faq-down-arrow-light.svg');
     }
@@ -69,8 +76,8 @@ function changeActiveFaqArrow(mediaQuery) {
   }
 }
 
-function changePhoneImages(mediaQueryDesktop) {
-  if (mediaQueryDesktop.matches) {
+function changePhoneImages(mediaQuery) {
+  if (mediaQuery.matches) {
     phoneMiddle.setAttribute('src', '/images/phone-middle-desktop.svg');
     phoneScreensSide[0].setAttribute('src', '/images/phone-1-screen-desktop.svg');
     phoneScreensSide[1].setAttribute('src', '/images/phone-3-screen-desktop.svg');
@@ -80,19 +87,19 @@ function changePhoneImages(mediaQueryDesktop) {
     phoneMiddle.setAttribute('src', '/images/phone-middle.svg');
     phoneScreensSide[0].setAttribute('src', '/images/phone-1-screen.svg');
     phoneScreensSide[1].setAttribute('src', '/images/phone-3-screen.svg');
-    instructionsHeading.innerHTML = "<span>Step 3. </span>Transfers to people from your contact list";
+    instructionsHeading.innerHTML = "<span>Step 3.</span> Transfers to people from your contact list";
     instructionsParagraph.innerHTML = "Proin volutpat mollis egestas. Nam luctus facilisis ultrices. Pellentesque volutpat ligula est. Mattis fermentum, at nec lacus.";
   }
 }
 
-let mediaQuery = window.matchMedia("(min-width: 768px)")
-changeActiveFaqArrow(mediaQuery);
+let mediaQueryTablet = window.matchMedia("(min-width: 768px)")
+changeActiveFaq(mediaQueryTablet);
 
 let mediaQueryDesktop = window.matchMedia("(min-width: 1400px)")
 changePhoneImages(mediaQueryDesktop);
 
-mediaQuery.addEventListener("change", function() {
-  changeActiveFaqArrow(mediaQuery);
+mediaQueryTablet.addEventListener("change", function() {
+  changeActiveFaq(mediaQueryTablet);
 });
 
 mediaQueryDesktop.addEventListener("change", function() {
